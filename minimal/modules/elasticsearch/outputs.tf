a416bab47e1dcb60282dcf28243bdd26b9e6c92e
Damien Laureaux <d.laureaux@timoa.com>
2019-01-14 14:47:47 +0000

    Add more documentation to the Elasticsearch sub-module outputs

--- minimal/modules/elasticsearch/outputs.tf
+++ minimal/modules/elasticsearch/outputs.tf
@@ -1,15 +1,19 @@
 output "elasticsearch_endpoint" {
-  value = "${aws_elasticsearch_domain.elasticsearch.endpoint}"
+  description = "Domain-specific endpoint used to submit index, search, and data upload requests"
+  value       = "${aws_elasticsearch_domain.elasticsearch.endpoint}"
 }
 
 output "elasticsearch_arn" {
-  value = "${aws_elasticsearch_domain.elasticsearch.arn}"
+  description = "Amazon Resource Name (ARN) of the domain"
+  value       = "${aws_elasticsearch_domain.elasticsearch.arn}"
 }
 
 output "elasticsearch_domain_id" {
-  value = "${aws_elasticsearch_domain.elasticsearch.domain_id}"
+  description = "Unique identifier for the domain"
+  value       = "${aws_elasticsearch_domain.elasticsearch.domain_id}"
 }
 
 output "elasticsearch_kibana_endpoint" {
-  value = "${aws_elasticsearch_domain.elasticsearch.kibana_endpoint}"
+  description = "Domain-specific endpoint for kibana without https scheme"
+  value       = "${aws_elasticsearch_domain.elasticsearch.kibana_endpoint}"
 }
