942934dce1ee94b89a1527ced8420192d78d9385
Damien Laureaux <d.laureaux@timoa.com>
2019-01-14 13:16:34 +0000

    Add the missing security sub-module (KMS)

---
+++ minimal/modules/security/main.tf
@@ -0,0 +1,13 @@
+resource "aws_kms_key" "elasticsearch_kms_key" {
+  description = "KMS key to encrypt the Elasticsearch volume"
+
+  # Tags
+  tags = "${merge(var.tags, map(
+    "Name", "elasticsearch-kms"
+  ))}"
+}
+
+resource "aws_kms_alias" "key" {
+  name          = "alias/elasticsearch-kms"
+  target_key_id = "${aws_kms_key.elasticsearch_kms_key.key_id}"
+}
